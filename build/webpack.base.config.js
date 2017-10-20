940d8172324ad273f87d8404a1c57c0b859a2f9b
Habib Khan <mkhk352@gmail.com>
2017-10-20 08:25:40 -0500

    workng changes so far

---
+++ build/webpack.base.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+
+const config = {
+    entry: {
+        app: path.resolve(__dirname, "../src/client-entry-app.js")
+    },
+    output:{
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/",
+        filename: "assests/js/[name].js"
+    }
+};
+
+
+module.exports = config;
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+
+const app = new Vue({
+    data: {
+        hello: "hi"
+    }
+});
+
+
+export { app };
