922a771faccc3584440a14cebe4e8af8ea47af72
Habib Khan <hkhan@smbhd.com>
2017-10-27 13:33:55 -0500

    added style to fix card edges

---
+++ src/theme/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="nav">
+        <div class="container">
+            <a href="/">
+            <img src="http://bit.ly/vue-img"
+                alt="Vue SPA"/>
+            </a>
+        </div>
+    </nav>
+</template>
