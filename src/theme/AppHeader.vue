6acbd1153081549ba8b1f2c6884bf4083631a238
Habib Khan <hkhan@smbhd.com>
2017-10-30 22:36:24 -0500

    cleaned up routing

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,10 +1,16 @@
 <template>
     <nav class="nav">
         <div class="container">
-            <a href="/">
+            <router-link to="/category/front-end" exact>
             <img src="http://bit.ly/vue-img"
                 alt="Vue SPA"/>
-            </a>
+            </router-link>
+            <router-link to="/category/front-end" class="nav-item is-tab" exact>
+              Home
+            </router-link>
+            <router-link to="/login"  class="nav-item is-tab">
+            Login
+            </router-link>
         </div>
     </nav>
 </template>
